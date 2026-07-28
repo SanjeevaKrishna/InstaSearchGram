fe32b980258c7090879cfb763a2a1052e9dd25fb
unknown <usanjeevakrishna@gmail.com>
2026-07-29 00:05:31 +0530

    Removed contact us bug

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -118,7 +118,7 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp.gmail.com',
       port: parseInt(process.env.SMTP_PORT || '587', 10),
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
